ffea988a0175648a47e4db422fea2f93ecfef796
nyein2001 <nyeinchantoe0@gmail.com>
2022-11-28 09:39:41 +0630

    Update Latest Feature

--- app/src/main/java/com/lightidea/products/letdrive/utils/Tools.java
+++ app/src/main/java/com/lightidea/products/letdrive/utils/Tools.java
@@ -99,18 +99,4 @@ public class Tools {
     public static void hideKeyboard(Activity context) {
         context.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
-
-    public static double getDistance(double lat1, double lon1, double lat2, double lon2) {
-        if ((lat1 == lat2) && (lon1 == lon2)) {
-            return 0;
-        }
-        else {
-            double theta = lon1 - lon2;
-            double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
-            dist = Math.acos(dist);
-            dist = Math.toDegrees(dist);
-            dist = dist * 60 * 1.1515;
-            return (dist);
-        }
-    }
 }
